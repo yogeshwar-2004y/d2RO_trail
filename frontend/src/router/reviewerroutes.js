0ba21280106e79d882b146460b5029c4cb752973
Mahadev-Manohar <mahadevmanohar07@gmail.com>
2025-09-02 15:13:41 +0530

    segregation finished

--- frontend/src/router/reviewerroutes.js
+++ frontend/src/router/reviewerroutes.js
@@ -1,8 +1,6 @@
 // src/router/reviewerroutes.js
 
 import HomePageReviewer from '@/views/reviewer/HomePageReviewer.vue'
-import InspectionMemo from '../../../z-extra/InspectionMemo.vue'
-import ReviewerMemoDashboard from '../../../z-extra/ReviewerMemoDashboard.vue'
 import SharedMemoDashboard from '@/views/reviewer/SharedMemoDashboard.vue'
 import SharedMemoView from '@/views/reviewer/SharedMemoView.vue'
 
@@ -11,16 +9,6 @@ const reviewerRoutes = [
     path: '/reviewer',
     name: 'HomePageReviewer',
     component: HomePageReviewer,
-  },
-  {
-    path: '/inspection-memo',
-    name: 'InspectionMemo',
-    component: InspectionMemo,
-  },
-  {
-    path: '/memo-dashboard',
-    name: 'ReviewerMemoDashboard',
-    component: ReviewerMemoDashboard,
   },
   {
     path: '/shared-memos',
